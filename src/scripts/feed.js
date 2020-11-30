a3f92367711603a79fc526b8f429ecabe6c6eb83
HenriqueCastros <henrique.castros@outlook.com>
2020-11-30 15:38:08 -0300

    ADD Desconnect

--- src/scripts/feed.js
+++ src/scripts/feed.js
@@ -39,6 +39,35 @@ function favoritesBox(img, title, id) {
     return box;
 }
 
+function prepCabecalho() {
+    if (localStorage.getItem("statusLogin") != "1" && localStorage.getItem("statusLogin") != "2" &&
+        sessionStorage.getItem("statusLogin") != "1" && sessionStorage.getItem("statusLogin") != "2") {
+        document.querySelector(".navConect").innerHTML = 
+                `<a class="nav-link responsivo" href="../src/login.html">Conectar</a>`;
+        document.querySelector(".login .dropdown").innerHTML = 
+                `<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="../src/login.html">Conectar</a>
+                </div>`;
+    } else {
+        document.querySelector(".navConect").innerHTML = 
+                `<a class="nav-link responsivo" href="#">Meu perfil</a>
+                <a class="nav-link responsivo" href="#">Desconectar</a>`;
+        document.querySelector(".login .dropdown").innerHTML = 
+                `<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Meu perfil</a>
+                    <a class="dropdown-item" href="#" onclick="Desconect()">Desconectar</a>
+                </div>`;
+    }
+}
+
+function Desconect(){
+    localStorage.setItem("statusLogin", "0");
+    sessionStorage.setItem("statusLogin", "0");
+    localStorage.setItem("userId", "0");
+    sessionStorage.setItem("userId", "0");
+    location = location;
+}
+
 if (localStorage.getItem("Favoritos") == null) {
     localStorage.setItem("Favoritos", "[]");
 }
@@ -137,6 +166,8 @@ function searchResult() {
 }
 
 window.onload = () => {
+
+    prepCabecalho();
 
     if (localStorage.getItem("statusLogin") != "1" && localStorage.getItem("statusLogin") != "2" &&
         sessionStorage.getItem("statusLogin") != "1" && sessionStorage.getItem("statusLogin") != "2") {
